f9551515414cdb72b4d713b07af6305920ff69a3
yusupovna1403 <yusupovna2003girl@gmail.com>
2022-12-24 19:07:54 +0500

    logic function17

--- logic17.py
+++ logic17.py
@@ -6,4 +6,13 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    digit1 = a % 10
+    digit2 = a // 10 % 10
+    digit3 = a // 100 % 10
+    digit4 = a // 1000 % 10
+    digit5 = a // 10000
+    answer = digit5 > digit4 and digit4 > digit3 and digit3 > digit2 and digit2 > digit1
+    return answer
+print(main(98764))
+print(main(29746))
+print(main(12345))
