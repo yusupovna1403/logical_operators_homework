5f0ec1d57e28a2ad5aaa160d3f23cf7bdde06da0
yusupovna1403 <yusupovna2003girl@gmail.com>
2022-12-24 19:08:38 +0500

    logic function18

--- logic18.py
+++ logic18.py
@@ -5,4 +5,12 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    digit1 = a % 10
+    digit2 = a // 10 % 10
+    digit3 = a // 100 % 10
+    digit4 = a // 1000 % 10
+    digit5 = a // 10000
+    answer = digit5 < digit4 and digit4 < digit3 and digit3 < digit2 and digit2 < digit1
+    return answer
+print(main(12347))
+print(main(76543))
