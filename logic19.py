64680c35dc6bafd4822a6f0cfb1114dc680fc62b
yusupovna1403 <yusupovna2003girl@gmail.com>
2022-12-27 10:40:33 +0500

    logic function19

--- logic19.py
+++ logic19.py
@@ -13,8 +13,8 @@ def main(x):
     d3 = x // 100
     a = x >= 10 and x <= 99 or x >= 100 and x <= 999
     result = d1 == d2 or d1 == d3
-    
-    
     return result
 print(main(12))
-print(main(221))
+print(main(11))
+print(main(121))
+print(main(321))
